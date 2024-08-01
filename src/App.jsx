05a47a758f6897183ff196dead3e25e89854c88d
RoziqovM <mroziqov494@gmail.com>
2024-08-02 02:32:31 +0500

    GKS video qoyildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Serticate from "./components/certificat/Serticate";
+import Gks from "./components/gks/GKS";
+import VideoPlayer from "./components/gksVideo/VideoPlayer";
 import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Topik from "./components/topik/Topik";
 
 export default function App() {
   return (
-    <div className="container mx-auto space-y-14">
+    <div className="container mx-auto space-y-14 xs:px-1 sm:px-0">
       <Navbar />
       <Header />
       <Topik />
       <Serticate />
+      <Gks />
+      <VideoPlayer />
     </div>
   );
 }
